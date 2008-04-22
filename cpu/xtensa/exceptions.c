a82c56ce70e195042d24038f2b379cf7b370c24a
Chris Zankel <chris@zankel.net>
2008-04-22 13:58:48 -0700

    xtensa: add rudimentary exception handling
    U-Boot was just dying quietly. We now print a message including
    the code for the exception cause and issue a reset.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- cpu/xtensa/exceptions.c
+++ cpu/xtensa/exceptions.c
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2007 Tensilica Inc.
+ * (C) Copyright 2008 Tensilica Inc.
  *
  * See file CREDITS for list of people who contributed to this
  * project.
@@ -11,7 +11,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -20,7 +20,30 @@
  * MA 02111-1307 USA
  */
 
+/*
+ * Exception handling.
+ *  We currently don't handle any exception and force a reset.
+ *  (Note that alloca is a special case and handled in start.S)
+ */
+
 #include <common.h>
+#include <command.h>
+#include <asm/string.h>
+#include <asm/regs.h>
+
+void unhandled_exception(struct pt_regs*);
+
+typedef void (*handler_t)(struct pt_regs*);
+
+handler_t exc_table[EXCCAUSE_LAST] = { 
+	[0 ... EXCCAUSE_LAST-1] unhandled_exception, 
+};
+
+void unhandled_exception(struct pt_regs *regs)
+{
+	printf("Unhandled Exception: EXCCAUSE = %d\n", regs->exccause);
+	panic("*** PANIC\n");
+}
 
 #ifdef CONFIG_USE_IRQ
 #error "Use of interrupts is not supported in Avnet/Xtensa ports"
